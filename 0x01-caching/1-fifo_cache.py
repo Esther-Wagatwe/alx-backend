724b4c8a71eb11184286f7c3e0e89b0dc8a87a6e
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-03 23:18:44 +0300

    Debugging

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from collections import OrderedDict
 """FIFOCache module that inherits from BaseCaching"""
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
