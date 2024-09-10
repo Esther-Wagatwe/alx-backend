a105bc1aff3a213824f57f14e9b513e019ef6250
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-10 15:37:44 +0300

    Corrections

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -46,29 +46,22 @@ def before_request() -> None:
 def get_locale() -> str:
     """
     Select the best match for the user's preferred language.
-    Follows the order of priority:
-    1. Locale from URL parameters
-    2. Locale from user settings
-    3. Locale from request headers
-    4. Default locale
+    Uses request.accept_languages to match against supported languages.
+    Checks if the 'locale' parameter is present in the request arguments
+    and if it is a supported locale.
     """
     locale = request.args.get('locale', None)
     if locale and locale in app.config['LANGUAGES']:
         return locale
-    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
-        return g.user['locale']
-    locale = request.accept_languages.best_match(app.config['LANGUAGES'])
-    if locale:
-        return locale
-    return app.config['BABEL_DEFAULT_LOCALE']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def index() -> str:
     """default route"""
     if g.user:
-        return render_template('6-index.html', username=g.user['name'])
-    return render_template("6-index.html",)
+        return render_template('5-index.html', username=g.user['name'])
+    return render_template("5-index.html",)
 
 
 # app.before_request(before_request)
